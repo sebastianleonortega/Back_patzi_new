017915b4847a9da84cfa403ed4ae618e80b78456
Sebastian Leon <juanleon@wposs.com>
2023-05-26 09:21:32 -0500

    new

---
+++ src/test/java/com/platzi/Market_new/MarketNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.Market_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
